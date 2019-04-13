657ec03f986ea02e30073df538666ba0987402a7
proobles <paulinarobles2021@u.northwestern.edu>
2019-04-12 20:51:58 -0500

    Update ultrasens.ino

--- Code/ultrasens/ultrasens.ino
+++ Code/ultrasens/ultrasens.ino
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "BluetoothSerial.h"
 #include <math.h>
 // defines pins numbers
+BluetoothSerial SerialBT;
 const int trigPin = 15;
 const int echoPin = 14;
 // defines variables
@@ -9,6 +11,7 @@ int distance;
 
 void setup() 
 {
+SerialBT.begin("test1"); //Bluetooth device name
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 Serial.begin(9600); // Starts the serial communication
@@ -28,6 +31,6 @@ duration = pulseIn(echoPin, HIGH);
 // Calculating the distance
 distance= duration*0.0133858/2;
 // Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance);
+SerialBT.print("Distance: ");
+SerialBT.println(distance);
 }
