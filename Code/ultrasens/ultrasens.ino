8c235035ff2eb481cfbbee1a55de8db2f007ce01
proobles <paulinarobles2021@u.northwestern.edu>
2019-04-12 19:04:07 -0500

    ultrasens

--- Code/ultrasens/ultrasens.ino
+++ Code/ultrasens/ultrasens.ino
@@ -6,12 +6,16 @@ const int echoPin = 14;
 // defines variables
 long duration;
 int distance;
-void setup() {
+
+void setup() 
+{
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 Serial.begin(9600); // Starts the serial communication
-}
-void loop() {
+} 
+
+void loop() 
+{
 // Clears the trigPin
 digitalWrite(trigPin, LOW);
 delayMicroseconds(2);
