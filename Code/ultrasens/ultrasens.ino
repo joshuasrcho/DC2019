c9a2079036df676f158915167e5d085d8ceaa1bc
Joshua Cho <seungcho2021@u.northwestern.edu>
2019-04-23 18:43:01 -0500

    Revert "Gripper"
    This reverts commit cf556330a0af359837fefedd703d744cfc261123.

--- Code/ultrasens/ultrasens.ino
+++ Code/ultrasens/ultrasens.ino
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include "BluetoothSerial.h"
+//#include "BluetoothSerial.h"
 #include <math.h>
 // defines pins numbers
-BluetoothSerial SerialBT;
+//BluetoothSerial SerialBT;
 const int trigPin = 15;
 const int echoPin = 14;
 // defines variables
 long duration;
 int distance;
-int c=0;
+
 void setup() 
 {
-SerialBT.begin("test1"); //Bluetooth device name
+//SerialBT.begin("test1"); //Bluetooth device name
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 Serial.begin(9600); // Starts the serial communication
@@ -19,7 +19,6 @@ Serial.begin(9600); // Starts the serial communication
 
 void loop() 
 {
-  
 // Clears the trigPin
 digitalWrite(trigPin, LOW);
 delayMicroseconds(2);
@@ -31,22 +30,7 @@ digitalWrite(trigPin, LOW);
 duration = pulseIn(echoPin, HIGH);
 // Calculating the distance
 distance= duration*0.0133858/2;
-
-// if (SerialBT.available())
-//    {  
-//        c = SerialBT.read();
-//        Serial.write(c);
-//    }
-// 
-//    // Keep reading from Arduino Serial Monitor and send to HC-05
-//    if (Serial.available())
-//    {
-//        c =  Serial.read();
-//        SerialBT.write(c);  
-//    }
-
 // Prints the distance on the Serial Monitor
-//SerialBT.print("Distance: ");
-
-SerialBT.println(distance);
+Serial.print("Distance: ");
+Serial.println(distance);
 }
