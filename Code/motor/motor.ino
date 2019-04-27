cdbfa3f4ba50a4c93badfeec9496db27702887b9
Joshua Cho <seungcho2021@u.northwestern.edu>
2019-04-27 17:40:45 -0500

    Update motor.ino

--- Code/motor/motor.ino
+++ Code/motor/motor.ino
@@ -1,29 +1,43 @@
-const int M1PWM = 3; // Motor 1 PWM (speed)
-const int M1DIR = 2; // Motor 1 direction 
+const int M1PWM = 15; // Motor 1 PWM (speed)
+const int M1DIR = 32; // Motor 1 direction 
 const int M2PWM = 4; // Motor 2 PWM (speed)
 const int M2DIR = 5; // Motor 2 direction 
 
+const int M1A = 26;
+const int M1B = 25;
+const int PWM1channel = 0;
+const int PWM2channel = 1;
+
+
+
 void setup() {
+  ledcSetup(PWM1channel,5000,8); // pwm channel, frequency, resolution in bits
+  ledcAttachPin(M1PWM,PWM1channel); // pin, pwm channel
   pinMode(M1DIR, OUTPUT);
-  pinMode(M1PWM, OUTPUT);
+  digitalWrite(M1DIR, LOW);
+
+  ledcSetup(PWM2channel,5000,8); // pwm channel, frequency, resolution in bits
+  ledcAttachPin(M2PWM,PWM2channel); // pin, pwm channel
   pinMode(M2DIR, OUTPUT);
-  pinMode(M2PWM, OUTPUT);
+  digitalWrite(M1DIR, LOW);
+
+  pinMode(M1A, INPUT);
+  pinMode(M1B, INPUT);
+
+  Serial.begin(115200);
 
 }
 
 void loop() {
-
+  Serial.println("forward");
   forward(255);
   delay(3000);
-  forward(100);
-  delay(3000);
-  turnLeft(75);
-  delay(3000);
-  turnRight(75);
-  delay(3000);
-  backward(100);
-  delay(3000);
-  stopMotor();
+  
+  Serial.println("angle");
+  turnAngle();
+
+  Serial.println("backward");
+  backward(255);
   delay(3000);
 
 }
@@ -32,9 +46,9 @@ void loop() {
 // this function takes parameter "speed" from 0 - 255
 void forward(int spd) {
   digitalWrite(M1DIR,HIGH);
-  analogWrite(M1PWM,spd);
+  ledcWrite(PWM1channel,spd);
   digitalWrite(M2DIR,LOW);
-  analogWrite(M2PWM,spd);
+  ledcWrite(PWM2channel,spd);
   delay(10);
 }
 
@@ -42,35 +56,56 @@ void forward(int spd) {
 // this function takes parameter "speed" from 0 - 255
 void backward(int spd) {
   digitalWrite(M1DIR,LOW);
-  analogWrite(M1PWM,spd);
+  ledcWrite(PWM1channel,spd);
   digitalWrite(M2DIR,HIGH);
-  analogWrite(M2PWM,spd);
+  ledcWrite(PWM2channel,spd);
 }
 
 // turn left
 // this function takes parameter "speed" from 0 - 255 
 void turnLeft(int spd){ 
   digitalWrite(M1DIR,HIGH);
-  analogWrite(M1PWM,spd);
+  ledcWrite(PWM1channel,spd);
   digitalWrite(M2DIR,HIGH);
-  analogWrite(M2PWM,spd);
+  ledcWrite(PWM2channel,spd);
 }
 
 // turn right
 // this function takes parameter "speed" from 0 - 255
 void turnRight(int spd){
   digitalWrite(M1DIR,LOW);
-  analogWrite(M1PWM,spd);
+  ledcWrite(PWM1channel,spd);
   digitalWrite(M2DIR,LOW);
-  analogWrite(M2PWM,spd);
+  ledcWrite(PWM2channel,spd);
 }
 
 // stop
 void stopMotor() {
-  analogWrite(M1PWM,0);
-  analogWrite(M2PWM,0);
+  ledcWrite(PWM1channel,0);
+  ledcWrite(PWM1channel,0);
 }
 
-
-
+void turnAngle(){
+  int aState, aLastState = 0;
+  int counter = 0;
+  int angle = 0;
+  while(counter < 20000){
+    aState = digitalRead(M1A);
+  
+    if (aState != aLastState){     
+      if (digitalRead(M1B) != aState) {
+         counter ++;
+         angle ++;
+         forward(255); 
+      }
+      else {
+         counter--;
+         angle --;
+         backward(255); 
+       }
+    }
+    aLastState = aState; 
+    Serial.println(counter); 
+  }
+}
 
