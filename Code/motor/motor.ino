80992178e84cff6c6d574fe19fa037c35d0da6ab
Joshua Cho <seungcho2021@u.northwestern.edu>
2019-05-04 02:50:53 -0500

    can drive forward and backward straight

--- Code/motor/motor.ino
+++ Code/motor/motor.ino
@@ -1,41 +1,33 @@
 const int M1PWM = 15; // Motor 1 PWM (speed)
-const int M1DIR = 32; // Motor 1 direction 
+const int M1DIR = 32; // Motor 1 direction
+const int M1Encoder = 4; // Motor 1 encoder 
 const int M2PWM = 26; // Motor 2 PWM (speed)
 const int M2DIR = 25; // Motor 2 direction 
-
-// Motor 1 encoder
-const int M1A = 4;
-const int M1B = 36;
-
-// Motor 2 encoder
+const int M2Encoder = 36; // Motor 2 encoder
 
 // Set up PWM channels
 const int PWM1channel = 0;
 const int PWM2channel = 1;
 
-int counter = 0;
-int currStateA;
-int prevStateA;
-String encdir = "";
-
-
+volatile int M1EncoderCount = 0;
+volatile int M2EncoderCount = 0;
 
 void setup() {
   // Motor 1 setup
   ledcSetup(PWM1channel,5000,8); // pwm channel, frequency, resolution in bits
   ledcAttachPin(M1PWM,PWM1channel); // pin, pwm channel
   pinMode(M1DIR, OUTPUT);
-  pinMode(M1A, INPUT);
-  pinMode(M1B, INPUT);
+  pinMode(M1Encoder, INPUT);
   digitalWrite(M1DIR, LOW);
-  prevStateA = digitalRead(M1A);
+  attachInterrupt(digitalPinToInterrupt(M1Encoder), countM1, RISING);
   
-
+  // Motor 2 setup
   ledcSetup(PWM2channel,5000,8); // pwm channel, frequency, resolution in bits
   ledcAttachPin(M2PWM,PWM2channel); // pin, pwm channel
   pinMode(M2DIR, OUTPUT);
+  pinMode(M2Encoder, INPUT);
   digitalWrite(M2DIR, LOW);
-
+  attachInterrupt(digitalPinToInterrupt(M2Encoder), countM2, RISING);
   Serial.begin(115200);
 
   
@@ -43,46 +35,80 @@ void setup() {
 }
 
 void loop() {
-  // Read the current state of M1A
-  currStateA = digitalRead(M1A);
-
-  // If the previous and the current state of M1A are different, then a pulse has occurred
-  if (currStateA != prevStateA){
-    if (digitalRead(M1B) != currStateA){
-      counter --;
-      encdir = "CCW";
-    } else{
-      counter ++;
-      encdir = "CW";
-    }
-    Serial.print("Direction: ");
-    Serial.print(encdir);
-    Serial.print(" counter: ");
-    Serial.println(counter);
-    prevStateA = currStateA;
-  }
+  forward(50,2000);
+  delay(2000);
+  backward(50,2000);
+  delay(2000);
 }
 
-void M1EncoderUpdate(){
-  
+void countM1(){
+  M1EncoderCount++;
+}
+
+void countM2(){
+  M2EncoderCount++;
 }
 
 // Go forward
-// this function takes parameter "speed" from 0 - 255
-void forward(int spd) {
+// this function takes parameter count
+void forward(int PWM, int count) {
   digitalWrite(M1DIR,LOW);
   digitalWrite(M2DIR,HIGH);
-  ledcWrite(PWM1channel,spd);
-  ledcWrite(PWM2channel,spd);
+  M1EncoderCount = 0;
+  M2EncoderCount = 0;
+  int totalCount = 0;
+  int masterPWM = PWM;
+  int slavePWM = PWM;
+  int error = 0;
+  int kp = 15;
+  while(totalCount < count){
+    ledcWrite(PWM1channel, masterPWM);
+    ledcWrite(PWM2channel, slavePWM);
+    error = M1EncoderCount - M2EncoderCount;
+    slavePWM += (error/kp);
+    if (slavePWM > 255){
+      slavePWM = 255;
+    } else if (slavePWM < 0){
+      slavePWM = 0;
+    }
+    totalCount += M1EncoderCount;
+    M1EncoderCount = 0;
+    M2EncoderCount = 0;
+    delay(100);
+  }
+  stopMotor();
+  Serial.print("totalCount: ");
+  Serial.println(totalCount);
 }
 
 // Go backward
 // this function takes parameter "speed" from 0 - 255
-void backward(int spd) {
+void backward(int PWM, int count) {
   digitalWrite(M1DIR,HIGH);
   digitalWrite(M2DIR,LOW);
-  ledcWrite(PWM1channel,spd);
-  ledcWrite(PWM2channel,spd);
+  M1EncoderCount = 0;
+  M2EncoderCount = 0;
+  int totalCount = 0;
+  int masterPWM = PWM;
+  int slavePWM = PWM;
+  int error = 0;
+  int kp = 1;
+  while(totalCount < count){
+    ledcWrite(PWM1channel, masterPWM);
+    ledcWrite(PWM2channel, slavePWM);
+    error = M1EncoderCount - M2EncoderCount;
+    slavePWM -= (error/kp);
+    if (slavePWM > 255){
+      slavePWM = 255;
+    } else if (slavePWM < 0){
+      slavePWM = 0;
+    }
+    totalCount += M1EncoderCount;
+    M1EncoderCount = 0;
+    M2EncoderCount = 0;
+    delay(100);
+  }
+  stopMotor();
 }
 
 // turn left
@@ -103,8 +129,8 @@ void turnRight(int spd){
   ledcWrite(PWM2channel,spd);
 }
 
-// stop
 void stopMotor() {
   ledcWrite(PWM1channel,0);
   ledcWrite(PWM2channel,0);
 }
+
